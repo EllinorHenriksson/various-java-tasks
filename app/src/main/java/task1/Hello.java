6283c52f5a9b94b995870a29eabea051c2cda722
Ellinor Henriksson <eh224kr@student.lnu.se>
2022-01-28 11:55:00 +0100

    Complete task1 and task2

---
+++ app/src/main/java/task1/Hello.java
@@ -0,0 +1,14 @@
+package task1;
+
+/**
+ * The classic "Hello World!" program.
+ */
+public class Hello {
+  /**
+   * Prints "Hello World!".
+   * @param args - Input (if available).
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ app/src/main/java/task2/Time.java
@@ -0,0 +1,34 @@
+package task2;
+
+import java.util.Scanner;
+
+/**
+ * A program that calculates the time (hours, minutes and seconds) from a given value of seconds.
+ */
+public class Time {
+  /**
+   * Translates seconds to hours, minutes and seconds.
+   * @param args - Input (if available).
+   */
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Enter the number of seconds: ");
+    int seconds = scan.nextInt();
+    
+    int minutes = 0;
+    int hours = 0;
+
+    if (seconds >= 60) {
+      minutes = seconds / 60;
+      seconds = seconds % 60;
+
+      if (minutes >= 60) {
+        hours = minutes / 60;
+        minutes = minutes % 60;
+      }
+    }
+
+    System.out.println("This corresponds to: " + hours + " h, " + minutes + " min and " + seconds + " s.");
+    scan.close();
+  }
+}
